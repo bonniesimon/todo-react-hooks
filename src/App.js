76f0597af1eb4b1b4e8a6dc66ba8a39177b0728f
Bonnie Simon <bonniesimon@protonmail.com>
2020-03-01 20:38:17 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const Todo = ({todo}) => {
+  return(
+  <div className="todo">{todo.text}</div>
+  )
+}
+
+
+const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return ;
+    addTodo(value);
+    setValue("");
+  }
+
+  return(
+    <form onSubmit = {handleSubmit}>
+      <input 
+        type="text"
+        value = {value}
+        onChange = {e => setValue(e.target.value)}
+        className = "input"
+        />
+    </form>
+  )
+}
+
+
 function App() {
+  const [todos, setTodos] = useState([
+    {text: "Meet linda maam"},
+    {text: "Learn React"},
+    {text : "Learn Photoshop"}
+  ]);
+
+
+  const addTodo = (value) => {
+    const newTodos = [...todos,{text:value}];
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key = {index}
+            index = {index}
+            todo = {todo}
+          />
+        ))}
+        <TodoForm addTodo = {addTodo} />
+      </div> 
     </div>
   );
 }
