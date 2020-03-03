43fb5807562695608179e051167e8b5b87855488
Bonnie Simon <bonniesimon@protonmail.com>
2020-03-03 07:12:37 +0530

    Added a navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="navbar-container">
+            
+        </div>
+    )
+}
+
+export default Navbar
