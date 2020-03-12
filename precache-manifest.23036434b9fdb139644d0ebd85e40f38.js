c641e21916ed4dc39a31065a22dafb9e1e44155c
Bonnie Simon <bonniesimon@protonmail.com>
2020-03-12 21:34:17 +0530

    Updates

---
+++ precache-manifest.23036434b9fdb139644d0ebd85e40f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56634856ff924ae89911e6f5cc92afaa",
+    "url": "/todo-react-hooks/index.html"
+  },
+  {
+    "revision": "68aef165e786fc2e7aa2",
+    "url": "/todo-react-hooks/static/css/main.c2a49541.chunk.css"
+  },
+  {
+    "revision": "0526ffa96d00c9d52e26",
+    "url": "/todo-react-hooks/static/js/2.c64fbc43.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-react-hooks/static/js/2.c64fbc43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68aef165e786fc2e7aa2",
+    "url": "/todo-react-hooks/static/js/main.a1a44e68.chunk.js"
+  },
+  {
+    "revision": "4d04259cccb473e62707",
+    "url": "/todo-react-hooks/static/js/runtime-main.ac4b00d5.js"
+  }
+]);
